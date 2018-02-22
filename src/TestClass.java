97bdca1d5179bc77537dd1533b41bc1d370b93d1
farzan <cosmiclush@gmail.com>
2018-02-21 22:00:13 -0600

    k

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,11 +3,9 @@ public class TestClass {
 	
 	public static void main(String args[]) {
 		System.out.println("hello"); //Farzan's commit
-<<<<<<< HEAD
 		//test
 		//comment from local
-=======
->>>>>>> master
+
 	}
 
 }
